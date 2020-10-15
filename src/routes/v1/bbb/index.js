cb2cf6fea2730a1aa054aaea9d5681eecdc86f9a
nathaliewi <nathi-w@web.de>
2020-10-15 13:56:45 +0200

    updated index.js

--- src/routes/v1/bbb/index.js
+++ src/routes/v1/bbb/index.js
@@ -3,12 +3,14 @@ import Router from 'express-promise-router';
 // routers
 import { administrationRouter } from './administration';
 import { monitoringRouter } from './monitoring';
+import { recordingRouter } from './recording';
 
 const bbbRouter = Router();
 const route = '/bbb';
 
 bbbRouter.use(route, administrationRouter);
 bbbRouter.use(route, monitoringRouter);
+bbbRouter.use(route, recordingRouter);
 
 bbbRouter.get(route, async (req, res, next) => {
 	await res.sendStatus(200);
